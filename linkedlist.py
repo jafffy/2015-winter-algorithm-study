bfa9bbcb6ca142492d2ba4e6e13f18738009ccd9
Jaewon Choi <jainersoer@ajou.ac.kr>
2015-01-11 15:37:21 +0900

    List class 추가

--- linkedlist.py
+++ linkedlist.py
@@ -3,3 +3,8 @@ class Node:
         self.data = 0
         self.next = None
 
+class List:
+    def __init__(self):
+        self.head = Node()
+        self.data = None
+        self.head.next = None 
