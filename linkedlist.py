1408f683c6392729be5fae878713c446e968a389
Jaewon Choi <jainersoer@ajou.ac.kr>
2015-01-12 19:43:29 +0900

    push() 함수에 head가 None일 떄에 대한 처리

--- linkedlist.py
+++ linkedlist.py
@@ -5,6 +5,32 @@ class Node:
 
 class List:
     def __init__(self):
-        self.head = Node()
-        self.data = None
-        self.head.next = None 
+        self.head = None
+        
+    def push(self, prev, data):
+        if self.head == None:
+            self.head = Node()
+            self.head.data = data
+            self.head.next = None
+
+            return
+
+        idx = self.head
+
+        while idx.next != prev and idx.next != None:
+            idx = idx.next
+
+        newNode = Node()
+        newNode.data = data;
+        newNode.next =  prev.next
+        idx.next = newNode
+
+    def pop(self, target):
+        if target == self.head:
+            self.head = self.head.next
+            return
+
+        idx = self.head
+
+        while idx.next != target and idx.next != None:
+            idx = idx.next    
