65bf7e6a0cf67e1c0d449bedcdabbc82896ee3db
Jaewon Choi <jainersoer@ajou.ac.kr>
2015-01-13 13:41:19 +0900

    linked list 내용 추가

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -20,36 +20,65 @@ void List::insert(Node* prev, int data)
     }
 
     if (prev == nullptr) {
-        newNode->next = head->next;
-        head->next = newNode;
-
-        return;
+        prev = head;
     }
 
-    
+    newNode->next = prev->next;
+    prev->next = newNode;
 }
 
 void List::remove(Node* target)
 {
+    if (target == nullptr) {
+        std::printf("target is nullptr\n");
+        return;
+    }
 
+    if (target == head) {
+        head = head->next;
+    }
+
+    for (auto idx = head;
+        idx->next != target && idx->next != nullptr;
+        idx = idx->next);
+
+    if (idx->next == nullptr) {
+        std::printf("we cannot find target\n");
+
+        return;
+    } else {
+        idx->next = target->next;
+    }
+
+    delete target;
 }
 
 void List::push_front(int data)
 {
-
+    insert(head, data);
 }
 
 void List::push_back(int data)
 {
+    auto idx = head;
 
+    while (idx->next != nullptr)
+        idx = idx->next;
+
+    insert(idx, data);
 }
 
 void List::pop_front()
 {
-
+    remove(head);
 }
 
 void List::pop_back()
 {
+    auto idx = head;
 
+    while (idx->next != nullptr)
+        idx = idx->next;
+
+    remove(head);
 }
